762f4476bc3b3c25f0413d656efb62b0d579d23a
anshusarkar <anshusarkar11@gmail.com>
2024-05-21 17:14:04 +0530

    echo

--- Zero.cpp
+++ Zero.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std; // That is called a namesapce and that get's used to avoid the use of std:: evry time
+using namespace std; // That is called a namesapce and that get's used to avoid the use of std:: every time
 
 int  main()
 {
