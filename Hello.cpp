d21fdb6b4650d80b5d5c61fd04568c6ab1551cf3
anshusarkar <anshusarkar11@gmail.com>
2024-05-25 20:13:50 +0530

    echo

--- Hello.cpp
+++ Hello.cpp
@@ -5,7 +5,7 @@ int main(int argc, const char** argv) {
     int var ;
 
     std::cout << "Hello, CPP!\n"; // << is called an inserion opetator in CPP 
-    std::cout << "User please enter an integr :\n-> ";
+    std::cout << "User please enter an integer :\n-> ";
     std:: cin >> var ; // It's called an extraction opetator
     std::cout << "Entered value is : " << var << std::endl; // std namespaces end line oprtator.
 
